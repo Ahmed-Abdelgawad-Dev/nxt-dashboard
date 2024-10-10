34069dfb6978239ea4635f887d3d94336deb2ff3
Ahmed Abdelgawad <55253019+Ahmed-Abdelgawad-Dev@users.noreply.github.com>
2024-10-10 20:50:26 +0300

    sync

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    return (<p className="text-3xl">Dashboard Page</p>)
+
+}
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -12,6 +12,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
         },
       )}
     >
+
       {status === 'pending' ? (
         <>
           Pending
